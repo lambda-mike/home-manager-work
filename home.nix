7947a9d0badcdfdac28bded141c53ab3591b3a34
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-08-14 10:15:11 +0100

    Move git settings to git.nix file

--- home.nix
+++ home.nix
@@ -108,21 +108,7 @@
     gpg = {
       enable = true;
     };
-    git = {
-      enable = true;
-      aliases = {
-        br = "branch";
-        ci = "commit";
-        co = "checkout";
-        dc = "diff --cached";
-        d = "diff";
-        lg = "log --pretty='%Cred%h%Creset | %C(yellow)%d%Creset %s %Cgreen(%cr)%Creset %C(cyan)[%an]%Creset' --graph";
-        so = "show --pretty='parent %Cred%p%Creset commit %Cred%h%Creset%C(yellow)%d%Creset %n%n%w(72,2,2) %s%n%n%w(72,0,0) %C(cyan)%an%Creset %Cgreen%ar%Creset'";
-        st = "status";
-        s = "status -s";
-      };
-      userName = "Michał Grygierzec";
-    };
+    git = import ./git.nix;
     jq = {
       enable = true;
     };
