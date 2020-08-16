fd3e84624338e2d74187188030a62b2eb6f1ab73
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-08-16 11:13:57 +0100

    Configure basic key bindings for XMonad

--- xmonad.nix
+++ xmonad.nix
@@ -6,40 +6,36 @@
   import System.Exit as E
   import           XMonad
   import qualified XMonad.StackSet as W
-  import           XMonad.Util.EZConfig(additionalKeys)
+  import           XMonad.Util.EZConfig (additionalKeysP)
 
   main = xmonad $
-    def
-      { terminal    = myTerminal
-      , modMask     = mod4Mask
-      , borderWidth = 2
-      , normalBorderColor  = "#cccccc"
-      , focusedBorderColor = "#00558c"
-      } `additionalKeys` myKeysList
+    (config `additionalKeysP` myKeysList)
     where
-      myTerminal = "alacritty"
+      config =
+        def
+          { terminal    = myTerminal
+          , modMask     = mod4Mask
+          , borderWidth = 2
+          , normalBorderColor  = "#cccccc"
+          , focusedBorderColor = "#00558c"
+          }
+      myTerminal =
+        "alacritty"
       myKeysList =
-        [ ((modMask              , xK_Return), spawn myTerminal)
-        , ((modMask              , xK_p     ), spawn "rofi -show run")
-        , ((modMask .|. shiftMask, xK_p     ), spawn "rofi -show window")
-          -- %! Close the focused window
-        , ((modMask .|. shiftMask, xK_q     ), kill)
-          -- %! Quit xmonad
-        , ((modMask .|. shiftMask, xK_e     ), io (E.exitWith E.ExitSuccess))
-          -- %! Reztart xmonad
-        , ((modMask              , xK_z     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
-          -- %! Swap the focused window and the master window
-        , ((modMask .|. shiftMask, xK_m     ), windows W.swapMaster)
-        -- TODO
-        -- , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        -- , ((0, xK_Print), spawn "scrot")
+        [ ("M-<Return>", spawn myTerminal     )
+        , ("M-S-q"     , kill)
+        , ("M-S-r"     , spawn restartXMonad )
+        , ("M-r"       , refresh)
+        , ("M-S-x"     , io (E.exitWith E.ExitSuccess))
+        , ("M-p"       , spawn "rofi -show run")
+        , ("M-S-p"     , spawn "rofi -show window")
+        , ("M-S-m"     , windows W.swapMaster )
         ]
-        ++
-        -- mod-{a,r,s} %! Switch to physical/Xinerama screens 1, 2, or 3
-        -- mod-shift-{a,r,s} %! Move client to screen 1, 2, or 3
-        [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-            | (key, sc) <- zip [xK_a, xK_r, xK_s] [0..]
-            , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+      restartXMonad =
+        "if type xmonad; then " ++
+        "xmonad --recompile && xmonad --restart; " ++
+        "else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"
+
 '' ;
   extraPackages = haskellPackages : [
     haskellPackages.xmonad-contrib
