3eb60dda63fe45772fc8e9171779e7227005cf2b
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-08-16 12:32:40 +0100

    Remap XMonad screens keybindings

--- xmonad.nix
+++ xmonad.nix
@@ -3,7 +3,7 @@
   enable = true;
   enableContribAndExtras = true;
   config = pkgs.writeText "xmonad.hs" ''
-  import System.Exit as E
+  import           System.Exit as E
   import           XMonad
   import qualified XMonad.StackSet as W
   import           XMonad.Util.EZConfig (additionalKeysP)
@@ -13,23 +13,37 @@
     where
       config =
         def
-          { terminal    = myTerminal
-          , modMask     = mod4Mask
-          , borderWidth = 2
+          { terminal           = myTerminal
+          , modMask            = mod4Mask
+          , borderWidth        = 2
           , normalBorderColor  = "#cccccc"
           , focusedBorderColor = "#00558c"
           }
       myTerminal =
         "alacritty"
       myKeysList =
-        [ ("M-<Return>", spawn myTerminal     )
-        , ("M-S-q"     , kill)
-        , ("M-S-r"     , spawn restartXMonad )
-        , ("M-r"       , refresh)
-        , ("M-S-x"     , io (E.exitWith E.ExitSuccess))
-        , ("M-p"       , spawn "rofi -show run")
-        , ("M-S-p"     , spawn "rofi -show window")
-        , ("M-S-m"     , windows W.swapMaster )
+        [ ("M-<Return>", spawn myTerminal               )
+        , ("M-S-q"     , kill                           )
+        , ("M-S-r"     , spawn restartXMonad            )
+        , ("M-r"       , refresh                        )
+        , ("M-S-x"     , io (E.exitWith E.ExitSuccess)  )
+        , ("M-p"       , spawn "rofi -show run"         )
+        , ("M-S-p"     , spawn "rofi -show window"      )
+        , ("M-S-m"     , windows W.swapMaster           )
+        -- TODO
+        -- , ((0, xK_Print), spawn "scrot")
+        -- , ((shift, xK_Print), spawn "sleep 0.2; scrot -u") -- window
+        -- , ((shift control, xK_Print), spawn "sleep 0.2; scrot -s") -- rect
+        ]
+        ++ myScreenKeybindings
+      -- mod-{n,e,i} %! Switch to physical screens 1, 2, or 3
+      -- mod-shift-{n,e,i} %! Move client to screen 1, 2, or 3
+      myScreenKeybindings =
+        [ ( mask ++ "M-" ++ [key]
+          , screenWorkspace scr >>= flip whenJust (windows . action)
+          )
+          | (key   , scr ) <- zip "nei" [0..]
+          , (action, mask) <- [ (W.view, "") , (W.shift, "S-") ]
         ]
       restartXMonad =
         "if type xmonad; then " ++
