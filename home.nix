333c716829fe94d639ba6c7cd938767f453623f2
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-08-18 14:08:07 +0100

    Fix GPG --gen-key

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@
       linuxPackages_5_7.virtualbox
       linuxPackages_5_7.virtualboxGuestAdditions
       neofetch
+      pinentry
       python38
       ripgrep
       rustup
@@ -37,10 +38,16 @@
     stateVersion = "20.09";
   };
 
-  services.screen-locker = {
-    enable = true;
-    inactiveInterval = 10;
-    lockCmd = "${pkgs.i3lock}/bin/i3lock -n -c 00558c";
+  services = {
+    gpg-agent = {
+      enable = true;
+      pinentryFlavor = "tty";
+    };
+    screen-locker = {
+      enable = true;
+      inactiveInterval = 10;
+      lockCmd = "${pkgs.i3lock}/bin/i3lock -n -c 00558c";
+    };
   };
 
   xsession.enable = true;
