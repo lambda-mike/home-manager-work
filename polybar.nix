3f1cea383fa2bbae7b3ec51bc5432b0a063cd51c
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-08-22 12:51:35 +0100

    Fix polybar script

--- polybar.nix
+++ polybar.nix
@@ -3,10 +3,9 @@
   enable = true;
   script = ''
     #! /usr/bin/env bash
-    set -eu
 
     # Terminate already running bar instances
-    killall -eq polybar
+    killall -eqw polybar
 
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
       MONITOR="$m" polybar --reload mainbar-xmonad -c ~/.config/polybar/config &
