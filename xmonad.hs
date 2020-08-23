5465d14dcfc3b45a4d0bdabb77cd85ebf3aa0ece
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-08-23 11:30:53 +0100

    [xmonad] Layouts: Add Dishes, remove Accordion

--- xmonad.hs
+++ xmonad.hs
@@ -7,7 +7,7 @@ import           XMonad.Hooks.EwmhDesktops (fullscreenEventHook)
 import           XMonad.Hooks.ManageDocks (avoidStruts)
 import           XMonad.Hooks.ManageHelpers (doCenterFloat, isDialog)
 import           XMonad.Hooks.SetWMName (setWMName)
-import           XMonad.Layout.Accordion (Accordion(Accordion))
+import           XMonad.Layout.Dishes (Dishes(Dishes))
 import           XMonad.Layout.NoBorders (smartBorders)
 import           XMonad.ManageHook (composeAll)
 import qualified XMonad.StackSet as W
@@ -95,7 +95,7 @@ myStartupHook = do
 myLayout = id
   $ avoidStruts
   $ smartBorders
-  $ tiled ||| Mirror tiled ||| Full ||| Accordion
+  $ tiled ||| Mirror tiled ||| Full ||| Dishes 2 (1/6)
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
