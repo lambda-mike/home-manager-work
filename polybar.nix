3ba0dd3cec372fbe17d7c40c005bf504bf8e2873
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-09-04 18:25:14 +0100

    Launch Polybar on primary monitor first

--- polybar.nix
+++ polybar.nix
@@ -14,7 +14,12 @@
     # Terminate already running bar instances
     killall -eqw polybar
 
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    # Launch polybar on primary monitor first
+    mprim=$(xrandr --query | grep " connected" | grep "primary" cut -d" " -f1)
+    MONITOR="$mprim" polybar --reload mainbar-xmonad &
+
+    # Launch polybar on other monitors
+    for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
       MONITOR="$m" polybar --reload mainbar-xmonad &
     done
 
