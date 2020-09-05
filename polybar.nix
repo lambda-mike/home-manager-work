da1964741a789a0e9bdd2f1a656e6706c78362a1
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-09-05 10:16:04 +0100

    Fix Polybar init script typo

--- polybar.nix
+++ polybar.nix
@@ -15,7 +15,7 @@
     killall -eqw polybar
 
     # Launch polybar on primary monitor first
-    mprim=$(xrandr --query | grep " connected" | grep "primary" cut -d" " -f1)
+    mprim=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
     MONITOR="$mprim" polybar --reload mainbar-xmonad &
 
     # Launch polybar on other monitors
