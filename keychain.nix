65692c0b5cd87a8164e0a30175bbe9273c9fc591
Michał Grygierzec <michal.grygierzec@protonmail.com>
2021-02-18 17:58:49 +0000

    [nix] Remove old keychain.nix

--- keychain.nix
+++
@@ -1,8 +0,0 @@
-{
-  agents = [ "ssh" ];
-  enable = true;
-  enableZshIntegration = false;
-  extraFlags = [ "--quick" "--quiet" ];
-  # FIXME Phase1 keys should contain an array of strings: [ "id_rsa" ]
-  keys = import ./keys.nix;
-}
