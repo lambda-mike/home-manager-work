7822442a6de2d0e05bf4b9301cb5a18878c272ae
Michał Grygierzec <michal.grygierzec@protonmail.com>
2021-04-11 11:02:37 +0100

    configuration.nix for toshiba

---
+++ configuration_sabios.nix
@@ -0,0 +1,97 @@
+# Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+    #boot.initrd.kernelModules = [ "i915" ];
+    # Boot loader (UEFI)
+    boot.loader.systemd-boot.configurationLimit = 7;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.timeout = 1;
+
+    # Networking
+    networking.useDHCP = false;
+    networking.interfaces.yours.useDHCP = true;
+    networking.networkmanager.enable = true;
+    networking.hostName = "sabios";
+
+    # Localization
+    i18n.defaultLocale = "pl_PL.UTF-8";
+    console.useXkbConfig = true;
+    time.timeZone = "Europe/London";
+
+    # Sound
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+
+    # Services
+    services.locate.enable = true;
+    services.openssh = {
+      enable = true;
+      forwardX11 = true;
+      passwordAuthentication = false;
+      permitRootLogin = "no";
+      startWhenNeeded = true;
+    };
+    services.thermald.enable = true;
+
+    # X
+    services.pantheon.apps.enable = true;
+    services.pantheon.contractor.enable = true;
+    services.xserver = {
+      desktopManager.pantheon.enable = true;
+      enable = true;
+      layout = "pl";
+
+      # Touchpad support
+      libinput.enable = true;
+
+      displayManager = {
+        autoLogin = {
+          enable = true;
+          user = "sabi";
+        };
+        #defaultSession = "none+xmonad";
+        lightdm = {
+          enable = true;
+          greeters.pantheon.enable = true;
+        };
+      };
+    };
+
+    virtualisation.docker = {
+      enable = true;
+      autoPrune.enable = true;
+    };
+
+    # users
+    users.users.sabi = {
+      createHome = true;
+      extraGroups =
+        [ "audio" "disk" "networkmanager" "vboxsf" "video" "wheel" ];
+      group = "users";
+      home = "/home/sabi";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHTP5Tlnc2kzPlJ5MQk9zHo4VIZhrYugTDJgy3OOPSD4 Toshiba Kitkowo''
+      ];
+      uid = 1000;
+    };
+
+    # Pkgs
+    environment.systemPackages = with pkgs; [
+      killall
+      lynx
+      networkmanagerapplet
+      wget
+      vim
+    ];
+
+}
