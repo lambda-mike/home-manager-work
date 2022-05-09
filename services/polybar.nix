20bc4b5eda95d792dcc64fc90661df5e4ab358a5
Michał Grygierzec <michal.grygierzec@protonmail.com>
2022-05-09 18:59:58 +0200

    [polybar] Add short bar, handle vertical screens

--- services/polybar.nix
+++ services/polybar.nix
@@ -19,6 +19,20 @@ in {
     script = ''
 #! /usr/bin/env bash
 
+isVertical() {
+  local monitor="$1"
+  local isPrimary="$(xrandr --query | grep "$monitor" | grep "primary")"
+  if [[ -n "$isPrimary" ]]; then
+    local resolutionIndex=4
+  else
+    local resolutionIndex=3
+  fi
+  local width=$(xrandr --query | grep "$monitor" | cut -d" " -f"$resolutionIndex" | cut -d"+" -f1 | cut -d"x" -f1)
+  local height=$(xrandr --query | grep "$monitor" | cut -d" " -f"$resolutionIndex" | cut -d"+" -f1 | cut -d"x" -f2)
+  local result
+  [[ "$width" -lt "$height" ]] && return
+}
+
 PATH=$PATH:${
                    with pkgs;
                    lib.makeBinPath [ coreutils gnugrep killall xorg.xrandr ]
@@ -31,13 +45,23 @@ killall -eqw polybar
 mprim=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
 log0="/tmp/polybar_$mprim.log"
 echo "-----" | tee $log0
-MONITOR="$mprim" polybar --reload mainbar-xmonad 2>&1 | tee $log0 & disown
+if isVertical "$mprim"; then
+  bar="shortbar-xmonad"
+else
+  bar="mainbar-xmonad"
+fi
+MONITOR="$mprim" polybar --reload "$bar" 2>&1 | tee $log0 & disown
 
 # Launch polybar on other monitors
 for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
   logm="/tmp/polybar_$m.log"
   echo "-----" | tee $logm
-  MONITOR="$m" polybar --reload mainbar-xmonad 2>&1 | tee $logm & disown
+  if isVertical "$m"; then
+    bar="shortbar-xmonad"
+  else
+    bar="mainbar-xmonad"
+  fi
+  MONITOR="$m" polybar --reload "$bar" 2>&1 | tee $logm & disown
 done
 
 echo "Polybar launched..."
