52f3d28aa9d48344859651198ea96833b418a794
Michał Grygierzec <michal.grygierzec@protonmail.com>
2022-05-09 23:19:39 +0200

    Add updated instructions for building custom iso

--- iso.nix
+++ iso.nix
@@ -7,11 +7,14 @@
 
     # stable
     # nix-channel --add https://nixos.org/channels/nixos-20.03 nixos-20.03
-    <nixos-20.03/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
-    <nixos-20.03/nixos/modules/installer/cd-dvd/channel.nix>
+    <nixos-21.11/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+    <nixos-21.11/nixos/modules/installer/cd-dvd/channel.nix>
   ];
 
   # FIXED Newer kernel contains updated wifi firmware
   # nix-build '<nixos-20.03/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix
-  boot.kernelPackages = pkgs.linuxPackages_5_7;
+  # nix-channel --add https://... nixos-21.11
+  # nix-channel --update nixos-21.11
+  # nix-build '<nixos-21.11/nixos>' -A config.system.build.isoImage -I nixos-config=/tmp/iso.nix -I nixos-21.11=/nix/var/nix/profiles/per-user/mike/channels/nixos-21.11/default.nix
+  boot.kernelPackages = pkgs.linuxPackages_5_15;
 }
