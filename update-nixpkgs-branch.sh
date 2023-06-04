26e2fdd1f1d5a89f3b82e1ccea6bf50d8cc8fb94
Michał Grygierzec <michal.grygierzec@protonmail.com>
2023-06-04 12:20:15 +0200

    [niv] improve script for updating the branch of nixpkgs

--- update-nixpkgs-branch.sh
+++ update-nixpkgs-branch.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-nix-shell --run "niv modify nixpkgs -a branch=${1}" niv.nix
+# Example how to run:
+# ./update-nixpkgs-branch.sh "nixos-23.05
+if [ -z "${1}" ];
+then
+  echo 'Run with the correct branch name: ./update-nixpkgs-branch.sh "nixos-23.05"' && exit 1
+else
+  nix-shell --run "niv modify nixpkgs -a branch=${1}" niv.nix
+fi
