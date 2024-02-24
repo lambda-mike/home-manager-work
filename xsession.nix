52ff357db8edb224418ecba5afd87ab132ff61c5
Michał Grygierzec <mgrygierzec@lunarenergy.com>
2024-02-24 23:42:13 +0100

    fix xsessions

--- xsession.nix
+++ xsession.nix
@@ -1,12 +1,25 @@
-{ pkgs, ... }:
-
-{
-  xsession = {
-    enable = true;
-    windowManager.command = ''
-      ${pkgs.leftwm}/bin/leftwm &
-      waitPID=$!
-      test -n "$waitPID" && wait "$waitPID"
-    '';
+{ config, pkgs, wm ? "xmonad", ... }:
+let
+  choice = if sessions ? wm then wm else "xmonad";
+  sessions = {
+    xmonad = {
+      xsession = {
+        enable = true;
+        initExtra = ''
+        autorandr -c
+          ${config.xdg.configHome}/fehbg &
+        '';
+     };
+    };
+    leftwm = {
+      xsession = {
+        enable = true;
+        windowManager.command = ''
+          ${pkgs.leftwm}/bin/leftwm &
+          waitPID=$!
+          test -n "$waitPID" && wait "$waitPID"
+        '';
+      };
+    };
   };
-}
+in sessions.${choice}
